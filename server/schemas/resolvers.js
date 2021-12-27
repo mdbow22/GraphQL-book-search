aae80785222daef7021f607e6ce1eae7dff3bd53
mdbow22 <mdbow22@gmail.com>
2021-12-26 20:41:52 -0500

    made createUser functionality with graphql

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -8,7 +10,13 @@ const resolvers = {
               })
         }
     },
-    
+    Mutation: {
+        createUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user};
+        }
+    }
 }
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,6 +7,7 @@ const typeDefs = gql`
         email: String!
         password: String
         savedBooks: [Book]
+        bookCount: Int
     }
     
     type Book {
