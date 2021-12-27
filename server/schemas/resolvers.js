1b5d5f5eb537fc65d0f19c9b164344bbd027cfbf
mdbow22 <mdbow22@gmail.com>
2021-12-27 14:40:02 -0500

    added all resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,6 +15,49 @@ const resolvers = {
             const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user};
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email: email });
+
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+              }
+            
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+    
+            const token = signToken(user);
+    
+            return { token, user };
+        },
+        saveBook: async (parent, { bookId }, context) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: bookId } } },
+                { new: true }
+              );
+
+            if(!updatedUser) {
+                throw new AuthenticationError('You must be logged in to save a book');
+            }
+
+            return updatedUser;
+        },
+        deleteBook: async (parent, { bookId }, context) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: bookId } } },
+                { new: true }
+              );
+
+            if(!updatedUser) {
+                throw new AuthenticationError('You must be logged into delete a book');
+            }
+
+            return updatedUser;
         }
     }
 }
