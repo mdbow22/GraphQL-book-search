d13b61648119533c2c334d99d430cd654d086dfb
mdbow22 <mdbow22@gmail.com>
2022-01-03 14:02:49 -0500

    built production version of front end

---
+++ client/build/precache-manifest.27794af8e03acedddb3083663f0ea946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e2acad9cdf38d3505cb18cc17c451c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf531461c1956f87e59f",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "031db59605616c386463",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "bf531461c1956f87e59f",
+    "url": "/static/js/2.35807912.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.35807912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "031db59605616c386463",
+    "url": "/static/js/main.efae7675.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
